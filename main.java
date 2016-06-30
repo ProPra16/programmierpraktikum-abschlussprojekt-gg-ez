f310d6e0f0d86e8b695eff8400e090ce92a07395
marcelg1909 <Marcel@Marcels-MBP-3.fritz.box>
2016-06-30 14:28:15 +0200

    first commit

---
+++ main.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Marcel on 22.06.16.
+ */
+public class main extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Projekt7.fxml"));
+        Parent root = loader.load();
+        Projekt7Controller controller = loader.getController();
+
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
