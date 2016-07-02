50d5207e43d6925bcec2b6e2fa39036d73378bd4
marcelg1909 <Marcel@Marcels-MBP-3.fritz.box>
2016-07-02 14:37:49 +0200

    update Layout
     added functions

--- de.hhu.propra16.projekt7/src/main/java/Projekt7Controller.java
+++ de.hhu.propra16.projekt7/src/main/java/Projekt7Controller.java
@@ -1,23 +1,26 @@
 import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 
-/**
- * Created by Marcel on 22.06.16.
- */
 public class Projekt7Controller{
 
     @FXML private TextArea textArea1;
+    @FXML private TextArea testTextArea1;
 
+    private static Path pathFile1;
+    private static Path pathTest1;
 
     @FXML
     public void openExercise() throws FileNotFoundException {
@@ -26,30 +29,105 @@ public class Projekt7Controller{
         Stage stage = new Stage();
 
         File file = fileChooser.showOpenDialog(stage);
+        Path path = file.toPath();
 
-        System.out.println(file.getName());
+        String[] geteilt = path.toString().split("\\.");
+        String testPathString = geteilt[0] + "_TEST." + geteilt[1];
+        Path testPath = Paths.get(testPathString);
 
-        Path p = file.toPath();
 
-        List<String> list;
+        writeInTextArea1(path);
 
+        List<String> listTest = new ArrayList<String>();
         try {
-            list = Files.readAllLines(p);
+            listTest = Files.readAllLines(testPath);
+            pathTest1=testPath;
+            for (String s: listTest) testTextArea1.appendText(s + "\n");
+        } catch (IOException e) {
+            System.out.println("Kein zugehöriger Test gefunden");
+        }
+    }
 
-            for (String s: list){
-                textArea1.appendText(s + "\n");
+    @FXML
+    public void close(){
+        main.closeProgram();
+    }
 
-            }
+    @FXML
+    public void openHelp(){
+        try {
+            Desktop.getDesktop().open(new File ("./Benutzerhandbuch.pdf"));
+        } catch (IOException e) {
+            System.out.println("Datei nicht gefunden");
+        }
+    }
 
+    @FXML
+    public void showAbout(){
+        aboutScreen.showAbout();
+    }
 
+    @FXML
+    public void saveExercise() {
+        String string1 = textArea1.getText();
+        String[] text1 = string1.split("\\n");
 
+        String string2 = testTextArea1.getText();
+        String[] text2 = string2.split("\\n");
+
+        ArrayList<String> ausgabe1 = new ArrayList<String>();
+        for (int i=0; i<text1.length; i++){
+            ausgabe1.add(text1[i]);
+        }
+        try {
+            Files.write(pathFile1, ausgabe1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> ausgabe2 = new ArrayList<String>();
+        for (int i=0; i<text2.length; i++){
+            ausgabe2.add(text2[i]);
+        }
+        try {
+            Files.write(pathTest1, ausgabe2);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void setTextArea1(String text){
-        textArea1.setText(text);
+    private void writeInTextArea1(Path p){
+        List<String> listFile = new ArrayList<String>();
+        try {
+            listFile = Files.readAllLines(p);
+            pathFile1=p;
+            for (String s: listFile) textArea1.appendText(s + "\n");
+        } catch (IOException e) {
+            System.out.println("Datei konnte nicht geladen werden");
+        }
+    }
+
+    public void setTextArea1Active(boolean active){
+        if (active) {
+            textArea1.setEditable(true);
+            textArea1.setStyle("-fx-text-fill: black;");
+            return;
+        } else {
+            textArea1.setEditable(false);
+            textArea1.setStyle("-fx-text-fill: darkgray;");
+        }
+    }
+
+    public void setTestArea1Active(boolean active){
+        if (active) {
+            testTextArea1.setEditable(true);
+            testTextArea1.setStyle("-fx-text-fill: black;");
+            return;
+        } else {
+            testTextArea1.setEditable(false);
+            testTextArea1.setStyle("-fx-text-fill: darkgray;");
+        }
+
     }
 
 }
