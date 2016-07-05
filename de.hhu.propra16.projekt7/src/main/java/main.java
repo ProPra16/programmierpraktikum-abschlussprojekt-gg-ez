b13c112be7b81018cc087b292dc1412111815258
yesArjan <yesArjanxbl@gmail.com>
2016-07-05 16:43:21 +0200

    fixed gradle virtual kata lib import moved layout.fxml to resources folder

--- de.hhu.propra16.projekt7/src/main/java/main.java
+++ de.hhu.propra16.projekt7/src/main/java/main.java
@@ -1,5 +1,4 @@
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -20,7 +19,7 @@ public class main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/Projekt7.fxml"));
         Parent root = loader.load();
-        Projekt7Controller controller = loader.getController();
+        //Projekt7Controller controller = loader.getController();
 
         Scene scene = new Scene(root);
 
