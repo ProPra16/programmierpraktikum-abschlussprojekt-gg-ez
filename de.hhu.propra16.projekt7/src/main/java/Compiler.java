35e7ec40fabdb433cc62fa785d60bee98ff0d5fc
Jon4sW <jonas@drsweber.de>
2016-07-07 00:16:14 +0200

    updated Compiler and implemented into the Controller

--- de.hhu.propra16.projekt7/src/main/java/Compiler.java
+++ de.hhu.propra16.projekt7/src/main/java/Compiler.java
@@ -26,7 +26,9 @@ public class Compiler {
         HashMap<String, String> testMap = currentEx.getTestsText();
         for (String key: classMap.keySet())  unitsList.add(new CompilationUnit(key, classMap.get(key), false));
         for (String key: testMap.keySet())   unitsList.add(new CompilationUnit(key, classMap.get(key), true));
-        compiler = CompilerFactory.getCompiler((CompilationUnit[]) unitsList.toArray());
+        CompilationUnit[] compUnitAr= new CompilationUnit[unitsList.size()];
+        for(int i=0;i<unitsList.size();i++) compUnitAr[i]=unitsList.get(i);
+        compiler = CompilerFactory.getCompiler(compUnitAr);
 
         compiler.compileAndRunTests();
         compileResult = compiler.getCompilerResult();
@@ -35,18 +37,36 @@ public class Compiler {
         return !compileResult.hasCompileErrors();
     }
 
+    // Returns a String of all CompileErrors
     public String getCompileError(){
         if(compileResult.hasCompileErrors()){
             String compilationResult ="";
             for(CompilationUnit compUnit : unitsList) {
                 Collection<CompileError> allErrors = compileResult.getCompilerErrorsForCompilationUnit(compUnit);
-                for(CompileError er : allErrors) compilationResult+= er.toString();
+                for(CompileError er : allErrors) compilationResult+= er.toString() + "\n";
 
             }
             return  compilationResult;
         }
         return null;
     }
+
+    //False if tests failed. True if tests succeeded
+    public boolean tryTests(){
+        return testResult.getNumberOfFailedTests() == 0;
+    }
+
+    //returns a String of all Failed Tests
+    public String getTestfailMessage(){
+        if(!this.tryTests()){
+            String testFailMessage ="";
+           Collection<TestFailure> allfails = testResult.getTestFailures();
+            for(TestFailure tFail: allfails) testFailMessage+= tFail.getMessage() + "\n";
+            return  testFailMessage;
+        }
+        return null;
+    }
+
 
 
 }
