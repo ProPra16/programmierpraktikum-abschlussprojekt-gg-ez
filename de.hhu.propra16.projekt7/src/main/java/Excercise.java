a1102d44e05dc03b499a78729a001b5ea4a810eb
marcelg1909 <Marcel@Marcels-MacBook-Pro-3.local>
2016-07-06 10:47:20 +0200

    update

---
+++ de.hhu.propra16.projekt7/src/main/java/Excercise.java
@@ -0,0 +1,181 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Excercise {
+
+    private File file;
+    private Document doc;
+
+    private DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    private DocumentBuilder builder = factory.newDocumentBuilder();
+
+    private String name;
+    private Element root;
+    private Node classes;
+    private Node tests;
+
+    /*
+     * Dieser Construktor erstellt eine leere XML Datei mit dem Aufbau:
+     *
+     * <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+     * <excercise>
+     *     <classes/>
+     *     <tests/>
+     * </excercise>
+     *
+     * Die Funktionen addDefaultPair und addPair fügen eine class & test paar hinzu
+     */
+    public Excercise(String name) throws ParserConfigurationException, TransformerException {
+        this.name = name;
+        this.doc = builder.newDocument();
+        this.root = doc.createElement("excercise");
+        this.root.setAttribute("name",name);
+        doc.appendChild(root);
+
+        this.classes = doc.createElement("classes");
+        this.tests   = doc.createElement("tests");
+
+        root.appendChild(classes);
+        root.appendChild(tests);
+
+        saveEx();
+    }
+
+    /*
+     * Konstruktor liest eine XML Datei ein
+     */
+    public Excercise(File file) throws ParserConfigurationException, IOException, SAXException {
+        this.file = file;
+        loadEx();
+    }
+
+    public void loadEx() throws ParserConfigurationException, IOException, SAXException {
+        this.doc = builder.parse(file);
+        this.root = doc.getDocumentElement();
+        this.name = root.getAttribute("name");
+
+        NodeList tmp = root.getElementsByTagName("classes");
+        this.classes = tmp.item(0);
+
+        tmp = root.getElementsByTagName("tests");
+        this.tests = tmp.item(0);
+    }
+
+    public void saveEx() throws TransformerException {
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = factory.newTransformer();
+        DOMSource src = new DOMSource(doc);
+
+        this.file = new File(name+".xml");
+
+        StreamResult fileResult = new StreamResult(file);
+        transformer.transform(src, fileResult);
+    }
+
+    public void addDefaultPair(String namePair) throws TransformerException {
+        addPair(namePair,
+                        "public class " + namePair + " {\n" +
+                        "}"
+                ,
+                        "import static org.junit.Assert.*;\n" +
+                        "import org.junit.Test;\n\n" +
+                        "public class " + namePair + "Test {\n\n" +
+                        "   @Test\n" +
+                        "   public void testSomething() {\n\n" +
+                        "   }\n" +
+                        "}"
+        );
+    }
+
+    public void addPair(String namePair, String classText, String testText) throws TransformerException {
+        Element classNode = doc.createElement("class");
+        classNode.setAttribute("name", namePair);
+        classNode.setTextContent(classText);
+        this.classes.appendChild(classNode);
+
+        Element testNode = doc.createElement("test");
+        testNode.setAttribute("name", namePair+"Test");
+        testNode.setTextContent(testText);
+        this.tests.appendChild(testNode);
+
+        saveEx();
+    }
+
+    /*
+     * Fügt eine class mit dem übergebenen Namen und Text hinzu
+     */
+    public void addClassNode(String nameClass, String text) throws ParserConfigurationException, TransformerException {
+        Element classNode = doc.createElement("class");
+        classNode.setAttribute("name", nameClass);
+        classNode.setTextContent(text);
+
+        this.classes.appendChild(classNode);
+        saveEx();
+    }
+
+    /*
+     * Fügt einen test mit dem übergebenen Namen und Text hinzu
+     */
+    public void addTestNode(String nameTest, String text) throws ParserConfigurationException, TransformerException {
+        Element testNode = doc.createElement("test");
+        testNode.setAttribute("name", nameTest);
+        testNode.setTextContent(text);
+
+        this.tests.appendChild(testNode);
+        saveEx();
+    }
+
+    /*
+     * Gibt eine ArrayListe mit Strings der classes zurück
+     * Ein String in der Liste = eine class
+     */
+    public ArrayList<String> getClassesText(){
+        NodeList classList = classes.getChildNodes();
+        ArrayList<String> classText = new ArrayList<>();
+
+        for(int i = 0; i < classList.getLength(); i++){
+            Node node = classList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                classText.add(element.getTextContent());
+            }
+        }
+
+        return classText;
+    }
+
+    /*
+     * Gibt eine ArrayListe mit Strings der tests zurück
+     * Ein String in der Liste = ein test
+     */
+    public ArrayList<String> getTestsText(){
+        NodeList testList = tests.getChildNodes();
+        ArrayList<String> testText = new ArrayList<>();
+
+        for(int i = 0; i < testList.getLength(); i++){
+            Node node = testList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                testText.add(element.getTextContent());
+            }
+        }
+
+        return testText;
+    }
+
+}
