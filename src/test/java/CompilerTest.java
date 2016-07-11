5e0427b8e3bad20d9a03512286115b2455879b32
Jon4sW <jonas@drsweber.de>
2016-07-12 00:31:55 +0200

    added more Tests to CompileTest

--- src/test/java/CompilerTest.java
+++ src/test/java/CompilerTest.java
@@ -18,17 +18,19 @@ public class CompilerTest {
     private  HashMap<String, String> classMap;
     private  HashMap<String, String> testMap;
     private boolean testsCompile;
+    private boolean noCompileError;
+    private Compiler testCompiler=new Compiler();
 
-    public CompilerTest(HashMap<String, String> inputClassMap, HashMap<String, String> inputTestMap, boolean inputCopmile){
+    public CompilerTest(HashMap<String, String> inputClassMap, HashMap<String, String> inputTestMap, boolean inputCopmile, boolean inputCompileErrorMessage){
         classMap=inputClassMap;
         testMap=inputTestMap;
         testsCompile=inputCopmile;
+        noCompileError =inputCompileErrorMessage;
     }
 
-
+//******************ACTUAL TESTS***************************************+
     @Test
     public void testTryCompiling(){
-        Compiler testCompiler=new Compiler();
         Exercise mockExercise = mock(Exercise.class);
         when(mockExercise.getClassMap()).thenReturn(classMap);
         when(mockExercise.getTestMap()).thenReturn(testMap);
@@ -37,9 +39,12 @@ public class CompilerTest {
         verify(mockExercise).getClassMap();
         verify(mockExercise).getTestMap();
 
+        assertEquals(noCompileError, null==testCompiler.getCompileError());
     }
 
 
+
+    //***********************ALL PARAMETERS*******************************************
     @Parameterized.Parameters
     public static Collection<Object[]> data(){
         //--------- First Parameters--------------
@@ -62,10 +67,28 @@ public class CompilerTest {
                         "}");
 
         //--------- Second Parameters--------------
+        // public [missing c]lass Class
+        HashMap<String, String> classmap2=new HashMap<String, String>();
+        classmap2.put("Class", "public lass Class {" +
+                "            public static int method() {" +
+                "                return 1;" +
+                "            }" +
+                "        }");
 
+        HashMap<String, String> testmap2= new HashMap<String, String>();
+        testmap2.put("MainTest", "import static org.junit.Assert.*;" +
+                "        import org.junit.Test;" +
+                "        public class MainTest {" +
+                "            @Test" +
+                "            public void testSomething2() {" +
+                "                int x = Class.method();" +
+                "                assertEquals(1, x);" +
+                "            } " +
+                "}");
 
         return Arrays.asList(new Object[][] {
-                {classmap1, testmap1, true}
+                {classmap1, testmap1, true, true},
+                {classmap2, testmap2, false, false}
         });
     }
 }
