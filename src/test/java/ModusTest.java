6ff140a749142faa4ffe8a512df9c74840455f2f
Sebastian Benecke <sebastian.benecke@uni-duesseldorf.de>
2016-07-14 20:01:58 +0200

    Add Tests for Modus class

---
+++ src/test/java/ModusTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by seben100 on 14.07.16.
+ */
+public class ModusTest {
+
+    @Test
+    public void testToCodeTest(){
+        Modus mode = new Modus(Modus.Mode.Test);
+        mode.nextModus();
+        assertEquals(Modus.Mode.Code, mode);
+    }
+
+    @Test
+    public void codeToRefTest(){
+        Modus mode = new Modus(Modus.Mode.Code);
+        mode.nextModus();
+        assertEquals(Modus.Mode.Refactor, mode);
+
+    }
+
+    @Test
+    public void refToTestTest(){
+        Modus mode = new Modus(Modus.Mode.Refactor);
+        mode.nextModus();
+        assertEquals(Modus.Mode.Test, mode);
+    }
+
+    @Test
+    public void codeToTestTest(){
+        Modus mode = new Modus(Modus.Mode.Code);
+        assertEquals(Modus.Mode.Test, mode.getPreviousMode());
+    }
+}
